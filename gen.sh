92810ba7d0b9bb1be15919f11bc503e6b4ccbaba
Andrzej Borucki <andrzej.borucki@coinfirm.io>
2017-08-22 15:18:37 +0200

    generated part of c++ hbase project

--- gen.sh
+++ gen.sh
@@ -1,2 +1,4 @@
 thrift --out java/hbase/src --gen java hbase1.thrift
 thrift --out java/hbase/src --gen java hbase2.thrift
+mkdir cpp/Hbase/gen-cpp
+thrift --out cpp/Hbase/gen-cpp --gen cpp hbase1.thrift
